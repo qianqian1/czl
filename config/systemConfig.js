0899fb512a1fd9e585ab98725df6dadc79e72fab
wu_qian <wu_qian@founder.com.cn>
2020-09-25 13:34:29 +0800

    test

---
+++ config/systemConfig.js
@@ -0,0 +1,5 @@
+window.PGIS_SYSCONFIG = {
+  title: '应急指挥',
+  proServerName: { founderYjzh: 'founder-yjzh', founderFresourcemanagement: 'founder-fresourcemanagement' },
+  mapConfig: { appUserId: 'admin', appKey: 'XMHSQ5101' }
+};
